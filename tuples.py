f858a4cc3a9a88b38ea8f07b1774e65a49375fc7
yuriyberezskyy <bereza95@gmail.com>
2020-04-27 18:17:23 -0400

    Done

---
+++ tuples.py
@@ -0,0 +1,13 @@
+my_info = ('Mike', 24, "Programmer")
+print(my_info)
+
+my_info = ("Andrew", 25, "Waiter")
+name, age, occupation = my_info
+print(name)
+name = "Mike"
+print(name)
+
+# one_tuple
+
+one_tuple = (3,)
+print(one_tuple)
